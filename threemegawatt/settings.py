32aa4485ad7a6a0978dc2201880e79c8637a021a
Shahul Hameed <beingshahul@gmail.com>
2020-01-21 22:26:26 +0100

    Configure celery and redis to handle the periodic task to update the data points.

--- threemegawatt/settings.py
+++ threemegawatt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import djcelery
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'plants.apps.PlantsConfig',
     # 3rd party apps
     'rest_framework',
+    'djcelery',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CELERY CONFIGURATION
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'

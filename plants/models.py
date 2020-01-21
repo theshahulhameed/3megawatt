262ef72378b5f335b944a04b43e01420c0e971a6
Shahul Hameed <beingshahul@gmail.com>
2020-01-21 19:41:13 +0100

    Created models Plant and DataPoint

--- plants/models.py
+++ plants/models.py
@@ -1,3 +1,28 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Plant(models.Model):
+    uid = models.UUIDField(
+        primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class DataPoint(models.Model):
+    plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+    energy_expected = models.DecimalField(max_digits=25, decimal_places=15)
+    energy_observed = models.DecimalField(max_digits=25, decimal_places=15)
+    irradiation_observed = models.DecimalField(
+        max_digits=25, decimal_places=15)
+    irradiation_expected = models.DecimalField(
+        max_digits=25, decimal_places=15)
+    reading_time = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.plant.name}'s data on {self.reading_time}"
